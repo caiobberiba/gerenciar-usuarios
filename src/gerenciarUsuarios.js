c7fe9e84266e96c4cba71157c50ac59124918209
Caio Bêribá <caioberiba@hotmail.com>
2025-09-09 23:07:00 -0300

    feat: atualizar estrutura de usuários para incluir nome e email, e ajustar testes correspondentes

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,11 +1,31 @@
-const usuarios = [ 'Maria', 'João', 'Ana', 'Carlos' ];
+const usuarios = [ 
+  { 
+    nome: 'Maria', 
+    email: 'maria@email.com'
+  }, 
+  { 
+    nome: 'João', 
+    email: 'joao@email.com' 
+  }, 
+  { 
+    nome: 'Ana', 
+    email: 'ana@email.com' 
+  }, 
+  { 
+    nome: 'Carlos', 
+    email: 'carlos@email.com' 
+  } 
+];
 
 function retornarUsuarios() {
   return usuarios;
 }
 
-function adicionarNovoUsuario(nome) {
-       usuarios.push(nome);
+function adicionarNovoUsuario(usuario) {
+  usuarios.push(usuario);
 }
 
-module.exports = { retornarUsuarios, adicionarNovoUsuario };
+module.exports = { 
+  retornarUsuarios, 
+  adicionarNovoUsuario 
+};
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -3,14 +3,24 @@ const { expect } = require ('chai');
 
 describe ('Testar as funções de Gestão de Usuários', function() {
   it ('Validar se posso adicionar um novo nome de usuário na lista', function() {
+        
         // 1. Adicionar novo nome na lista de usuário
-        adicionarNovoUsuario('Pedro');
+        adicionarNovoUsuario({
+          nome: 'Pedro',
+          email: 'pedro@email.com'
+        });
+        adicionarNovoUsuario({
+          nome: 'Juliana',
+          email: 'juliana@email.com'
+        });
 
         // 2. Retornar a lista de usuários na caixa de lista de usuários
         const ListaDeUsuarios = retornarUsuarios();
-
+        
         // 3. Comparar se o novo nome foi adicionado no fim da lista de usuários
-        expect(ListaDeUsuarios.at(-1)).to.be.equal('Pedro');
-    });
+        expect(ListaDeUsuarios.at(-1)).to.eql({
+          nome: 'Juliana',
+          email: 'juliana@email.com'
+        });
+  });
 });
-
